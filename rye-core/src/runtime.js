89ccee058eff0a8f47a715bd5e3dfb7eedea3175
Martin Feineis <martinfeineis@gmx.de>
2018-05-26 20:45:04 +0200

    Simplified runtime implementation

--- rye-core/src/runtime.js
+++ rye-core/src/runtime.js
@@ -9,10 +9,10 @@ export function configureRuntime(configureLoader, customElements, setTimeout) {
     };
 
     // FIXME: OMG, side-effects in a filter...
-    const dispatchQueue = (state, meta) => {
+    const dispatchQueue = meta => {
         state.queue = state.queue.filter(({ element, next }) => {
             if (meta.element === element) {
-                setTimeout(() => requestPagelet(state, element, next), 0);
+                setTimeout(() => requestPagelet(element, next), 0);
                 return false;
             }
 
@@ -21,47 +21,45 @@ export function configureRuntime(configureLoader, customElements, setTimeout) {
     };
 
     // FIXME: Validation, validation, validation...
-    const api = {
-        define: (meta) => {
-            console.log('runtime.declare', meta);
+    const define = meta => {
+        console.log('runtime.declare', meta);
 
-            if (state.elements[meta.element]) {
-                console.error(`runtime.declare: "${meta.element}" is already defined`);
-                return;
+        if (state.elements[meta.element]) {
+            console.error(`runtime.declare: "${meta.element}" is already defined`);
+            return;
+        }
+
+        if (!validateMeta(meta)) {
+            console.error(`runtime.declare: meta for "${meta.element}" invalid`);
+            return;
+        }
+
+        state.elements[meta.element] = {
+            meta,
+        };
+
+        customElements.define(meta.element, class extends HTMLElement {
+            // FIXME: This should probably clean up after itself
+            connectedCallback() {
+                // FIXME
+                // Should we provide a default loading indicator
+                // for the time it takes to load the pagelet.
+                // A placeholder would be better but how does
+                // the loader know what to display? Maybe this is
+                // something a registration could provide optionally.
+                // After the pagelet factory is called the pagelet
+                // itself can take over.
+                requestPagelet(meta.element, factory => (
+                    factory(this)
+                ));
             }
+        });
 
-            if (!validateMeta(meta)) {
-                console.error(`runtime.declare: meta for "${meta.element}" invalid`);
-                return;
-            }
-
-            state.elements[meta.element] = {
-                meta,
-            };
-
-            customElements.define(meta.element, class extends HTMLElement {
-                // FIXME: This should probably clean up after itself
-                connectedCallback() {
-                    // FIXME
-                    // Should we provide a default loading indicator
-                    // for the time it takes to load the pagelet.
-                    // A placeholder would be better but how does
-                    // the loader know what to display? Maybe this is
-                    // something a registration could provide optionally.
-                    // After the pagelet factory is called the pagelet
-                    // itself can take over.
-                    requestPagelet(state, meta.element, factory => (
-                        factory(this)
-                    ));
-                }
-            });
-
-            dispatchQueue(state, meta);
-        },
+        dispatchQueue(state, meta);
     };
 
     // TODO: This is actually not so bad but needs refinement
-    const requestPagelet = (state, element, next) => {
+    const requestPagelet = (element, next) => {
         console.log("requestPagelet", element);
 
         const registration = state.elements[element];
@@ -84,6 +82,8 @@ export function configureRuntime(configureLoader, customElements, setTimeout) {
         use([registration.meta.element], next);
     };
 
-    return api;
+    return {
+        define,
+    };
 };
 
